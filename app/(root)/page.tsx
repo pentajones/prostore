3f26c9681da31a53f2d91c792772b4fab59bb014
pentajones <pentajones@gmail.com>
2025-04-25 18:34:54 +0200

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+/* export const metadata = {
+  title: 'Home',
+}; */
+
+import ProductList from '@/components/shared/product/product-list';
+import { getLatestProducts } from '@/lib/actions/product.actions';
+
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
+  return (
+    <>
+      <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
+    </>
+  );
+};
+
+export default Homepage;
